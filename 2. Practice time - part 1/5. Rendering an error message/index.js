173b5b242c0d8f54a6e14ca8d83b71103d9c59b9
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-07 02:20:04 +0530

    Update index.js

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,5 +1,8 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
-
-
+let errorOccurred = document.getElementById("error")
+function purchase() {
+  console.log("Button is clicked!")
+  errorOccurred.textContent = "Something went wrong, please try again"
+}
