b2ff9aa5c292ac33cc9c832d9bfd8ea3098e283d
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-06 23:33:26 +0530

    Update index.js

--- 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
+++ 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
@@ -1,11 +1,11 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
+let name = "Syed Farajuddin "
 let greeting = "Welcome back "
 
 welcomeEl.innerText = greeting + name
 
-// Add an emoji to the end!
+// Add an emoji to the end! 
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
-
+welcomeEl.innerText += "👋"
