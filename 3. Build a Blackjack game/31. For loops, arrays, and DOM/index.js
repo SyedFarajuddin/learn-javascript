1ca0cf71302a0f03f51057e3b1677bd4cd163bed
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-07 23:59:16 +0530

    Update index.js

--- 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
+++ 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
@@ -2,4 +2,12 @@ let sentence = ["Hello ", "my ", "name ", "is ", "Per"]
 let greetingEl = document.getElementById("greeting-el")
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
+for (i = 0; i < sentence.length; i++) {
+greetingEl.textContent += sentence[i]
+}
 
+// How do you keep the spaces between the words if i remove them from the array?
+
+// for (i = 0; i < sentence.length; i++) {
+// greetingEl.textContent += sentence[i] + " "
+// }
