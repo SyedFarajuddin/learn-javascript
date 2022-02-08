0a57a16b56fbfd7351db0dc603199dc2f36dab97
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-08 20:26:39 +0530

    Update index.js

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,11 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
-
+let castle = {
+  title: "San Giusto Abbey",
+  price: 173,
+  isSuperHost: true,
+  rooms: ["4 guests", "2 bedrooms", "2 beds", "2 baths"]
+}
+console.log(castle.title)
+console.log(castle.rooms)
