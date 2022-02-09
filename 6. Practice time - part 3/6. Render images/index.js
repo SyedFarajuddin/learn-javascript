d792d1a6ae0e5d54b75f957e408164824f243ae9
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-10 00:49:45 +0530

    Update index.js

--- 6. Practice time - part 3/6. Render images/index.js
+++ 6. Practice time - part 3/6. Render images/index.js
@@ -1,9 +1,16 @@
 // Create a function that renders the three team images
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
-
+let container = document.getElementById("container")
 const imgs = [
     "images/hip1.jpg",
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
+
+function renderImages {
+    for (i = 0; i < imgs.length; i++) {
+        container.innerHtml += `<img class="team-img" src=${imgs[i]}>`
+        }
+}
+        renderImages()
