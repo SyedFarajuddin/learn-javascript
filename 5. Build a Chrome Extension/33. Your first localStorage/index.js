5c75cddbe21434a8f723f5a34990a4f42ab5fb05
SyedFarajuddin <93252978+SyedFarajuddin@users.noreply.github.com>
2022-02-09 16:29:28 +0530

    Update index.js

--- 5. Build a Chrome Extension/33. Your first localStorage/index.js
+++ 5. Build a Chrome Extension/33. Your first localStorage/index.js
@@ -6,6 +6,9 @@ const ulEl = document.getElementById("ul-el")
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
 // 3. Clear localStorage
+let name = localStorage.setItem("myName", "Syed Farajuddin")
+console.log(localStorage.getItem(name))
+localStorage.clear()
 
 // HINTS:
 // localStorage.setItem(key, value)
